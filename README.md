840746771507170d9ce365bcd0662579a8265945
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-07 21:47:17 +0700

    add patch center

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dmenu - Dynamic Menu
+# Burhan Build of Dynamic Menu
 
 dmenu is an efficient dynamic menu for X. from [suckless.org](https://tools.suckless.org/dmenu/)
 
@@ -7,3 +7,7 @@ dmenu is an efficient dynamic menu for X. from [suckless.org](https://tools.suck
     git clone https://github.com/04burhanuddin/dmenu.git
     cd dmenu
     sudo make clean install
+
+## Patch & Features
+
+- [Center](https://tools.suckless.org/dmenu/patches/center/) - This patch centers dmenu in the middle of the screen.
--- config.h
+++ config.h
@@ -2,10 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"JetbrainsMono Nerd Font:size=10:antialias=true",
-	"JetbrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true"
+static const char *fonts[] = { 
+  "JetbrainsMono Nerd Font:size=10:antialias=true",
+  "JetbrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
