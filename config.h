96119afc01dae43380bec51d4ac827460676385e
04burhanuddin <dev.burhanuddin@gmail.com>
2024-02-01 08:00:35 +0700

    new colors inspired by solarized osaka

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#FFFFFF", "#00141A" },
+	[SchemeSel] = { "#48AEF5", "#00141A" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
